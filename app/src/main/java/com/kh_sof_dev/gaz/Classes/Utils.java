8aca29be2ef1c89828ea618c221fd17fc7adc3d6
AhmedJAbuSada <ahmedj.abusada@gmail.com>
2019-08-16 18:24:42 +0300

    v1 16/08/2019

--- app/src/main/java/com/kh_sof_dev/gaz/Classes/Utils.java
+++ app/src/main/java/com/kh_sof_dev/gaz/Classes/Utils.java
@@ -1,6 +1,7 @@
 package com.kh_sof_dev.gaz.Classes;
 
 import android.content.Context;
+import android.os.Build;
 import android.text.TextUtils;
 import android.widget.ImageView;
 
@@ -9,17 +10,25 @@ import com.squareup.picasso.Picasso;
 public class Utils {
 
     public static void showImage(Context context, String url, ImageView imageView) {
-        if (url != null && !TextUtils.isEmpty(url))
+        if (url != null && !TextUtils.isEmpty(url)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !url.startsWith("https")) {
+                url = url.replace("http", "https");
+            }
             Picasso.with(context)
                     .load(url)
                     .into(imageView);
+        }
     }
 
     public static void showImage(Context context, String url, int placeholder, ImageView imageView) {
-        if (url != null && !TextUtils.isEmpty(url))
+        if (url != null && !TextUtils.isEmpty(url)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !url.startsWith("https")) {
+                url = url.replace("http", "https");
+            }
             Picasso.with(context)
                     .load(url)
                     .placeholder(placeholder)
                     .into(imageView);
+        }
     }
 }
